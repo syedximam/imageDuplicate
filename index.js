d73ac6ee375a43a9a588282bdd0f141004e36abd
syedximam <syedximam@gmail.com>
2025-08-04 17:47:46 +0000

    Hashed and Compared

--- index.js
+++ index.js
@@ -1,8 +1,42 @@
 const express = require('express')
+const crypto = require('crypto');
+const fs = require('fs');
 const multer = require('multer') // Install multer
 const path = require('path')
 const app = express()
 const port = 3000
+
+
+// Function for hashing images
+function hashing() {
+    const hashes = [];
+    const folderName = path.join(__dirname, 'uploads/');
+    const files = fs.readdirSync(folderName);
+
+
+    for (let i = 0; i < files.length; i++) {
+        const fullPath = path.join(folderName, files[i]);
+        const stat = fs.statSync(fullPath);
+        if (stat.isFile()) {
+            
+            const hash = crypto.createHash('md5').update(fs.readFileSync(fullPath)).digest('hex');
+            hashes.push(hash);
+        }
+    }
+    return hashes;
+}
+
+function compare(array) {
+    const duplicates = [];
+    for (let i = 0; i < array.length; i++) {
+        for (let j = i + 1; j < array.length; j++) {
+            if (array[i] === array[j]) {
+                duplicates.push(array[i]);
+            }
+        }
+    }
+    return duplicates;
+}
 
 // Storing the uploaded files with same extension in uploads folder
 const storage = multer.diskStorage({
@@ -23,11 +57,14 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'))
 })
 app.post("/profile", upload.array('image'), (req, res) => {
-    const abb = req.files
-    res.send({ msg: abb });
+    const hashed = hashing();
+    console.log(hashed);
+    const compared = compare(hashed);
+    console.log(compared)
+    res.send({ msg: `You have ${compared.length} duplicate files ${compared}`});
 })
- 
+
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
